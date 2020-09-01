30e9d8f317cc53ed459618c67a5e96a4de6508fb
Dan Rollo <danrollo@gmail.com>
2020-09-01 17:32:38 -0400

    minimal script

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd $GITHUB_WORKSPACE
-/usr/local/nancy $1
+#cd $GITHUB_WORKSPACE
+#/usr/local/nancy $1
+$1
