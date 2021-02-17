bc8c8479f99487c004e1199b90cbb6cde010ff28
Dan Rollo <danrollo@gmail.com>
2021-02-17 16:13:58 -0500

    detect malformed version string

--- install-nancy.sh
+++ install-nancy.sh
@@ -19,11 +19,14 @@ echo "desiredVersion: ${desiredVersion}"
 if [ -z "$desiredVersion" ]; then
   >&2 echo "must specify a desiredVersion, like: latest or v1.0.15"
   exit 1
-fi
-if [[ ${desiredVersion} == "latest" ]]; then
+elif [[ ${desiredVersion} == "latest" ]]; then
   latest_version_is=$(curl --fail -s https://api.github.com/repos/sonatype-nexus-community/nancy/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
   desiredVersion=${latest_version_is}
+elif [[ $desiredVersion != v* ]]; then
+  >&2 echo "specific version must start with v, like: v1.0.15"
+  exit 1
 fi
+# installer filename excludes v from version
 sourceUrl="https://github.com/sonatype-nexus-community/nancy/releases/download/${desiredVersion}/nancy_${desiredVersion:1}_linux_386.apk"
 echo "installing nancy via ${sourceUrl}"
 curl --fail -L -o nancy.apk ${sourceUrl}
