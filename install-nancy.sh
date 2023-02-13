3b0a989791377b50913568102c0ed5539e4eb29c
Patrik <zepatrik@users.noreply.github.com>
2023-02-13 16:46:00 +0100

    feat: authenticate github API calls

--- install-nancy.sh
+++ install-nancy.sh
@@ -14,13 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+acurl() {
+  curl "$@"
+}
+if [ -n "${INPUT_GITHUBTOKEN}" ]; then
+  acurl() {
+    echo "using authenticated curl"
+    curl -H "Authorization: Bearer ${INPUT_GITHUBTOKEN}" "$@"
+  }
+fi
+
 desiredVersion="$1"
 echo "desired nancy version: ${desiredVersion}"
 if [ -z "$desiredVersion" ]; then
   >&2 echo "must specify a desiredVersion, like: latest or v1.0.15"
   exit 1
 elif [[ ${desiredVersion} == "latest" ]]; then
-  latest_version_is=$(curl --fail -s https://api.github.com/repos/sonatype-nexus-community/nancy/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+  latest_version_is=$(acurl --fail -s https://api.github.com/repos/sonatype-nexus-community/nancy/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
   desiredVersion=${latest_version_is}
 elif [[ ${desiredVersion:0:1} != "v" ]]; then
   >&2 echo "specific nancy version (${desiredVersion}) must start with v, like: v1.0.15"
@@ -29,5 +39,5 @@ fi
 # installer filename excludes v from version
 sourceUrl="https://github.com/sonatype-nexus-community/nancy/releases/download/${desiredVersion}/nancy_${desiredVersion:1}_linux_amd64.apk"
 echo "installing nancy via ${sourceUrl}"
-curl --fail -L -o nancy.apk ${sourceUrl}
+acurl --fail -L -o nancy.apk "${sourceUrl}"
 apk add --no-progress --quiet --no-cache --allow-untrusted nancy.apk
